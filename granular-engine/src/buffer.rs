769fd8ec504d12e030fa73d5dcbecfc7dd53cec8
Daniel Friedman <dfriedman58@gmail.com>
2024-09-01 13:25:19 -0400

    Remove unused frame-based buffer iterators

--- granular-engine/src/buffer.rs
+++ granular-engine/src/buffer.rs
@@ -11,7 +11,7 @@ impl<const C: usize> Buffer<C> {
         Self::new_with_capacity(len, len)
     }
 
-    pub fn new_from(len: usize, data: [Vec<f32>; C]) -> Self {
+    pub(crate) fn new_from(len: usize, data: [Vec<f32>; C]) -> Self {
         Self { len, data }
     }
 
@@ -36,34 +36,6 @@ impl<const C: usize> Buffer<C> {
         frame
     }
 
-    // TODO is this remotely useful?
-    // pub fn frames(&self) -> Frames<'_, C> {
-    //     // TODO prevent allocations
-    //     Frames {
-    //         iters: self
-    //             .data
-    //             .iter()
-    //             .map(|v| v.iter())
-    //             .collect::<Vec<_>>()
-    //             .try_into()
-    //             .unwrap(),
-    //     }
-    // }
-
-    // TODO is this remotely useful?
-    // pub fn frames_mut(&mut self) -> FramesMut<'_, C> {
-    //     // TODO prevent allocations
-    //     FramesMut {
-    //         iters: self
-    //             .data
-    //             .iter_mut()
-    //             .map(|v| v.iter_mut())
-    //             .collect::<Vec<_>>()
-    //             .try_into()
-    //             .unwrap(),
-    //     }
-    // }
-
     pub fn resize(&mut self, new_capacity: usize, new_len: usize) {
         assert!(new_capacity >= new_len);
         if new_capacity > self.capacity() {
@@ -80,43 +52,3 @@ impl<const C: usize> Buffer<C> {
 }
 
 pub type StereoBuffer = Buffer<2>;
-
-struct Frames<'a, const C: usize> {
-    iters: [slice::Iter<'a, f32>; C],
-}
-
-impl<'a, const C: usize> Iterator for Frames<'a, C> {
-    type Item = [f32; C];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut result = [0.; C];
-        for (i, it) in self.iters.iter_mut().enumerate() {
-            if let Some(&sample) = it.next() {
-                result[i] = sample;
-            } else {
-                return None;
-            }
-        }
-        Some(result)
-    }
-}
-
-struct FramesMut<'a, const C: usize> {
-    iters: [slice::IterMut<'a, f32>; C],
-}
-
-impl<'a, const C: usize> Iterator for FramesMut<'a, C> {
-    type Item = [&'a mut f32; C];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut result: [Option<&mut f32>; C] = array::from_fn(|_| None);
-        for (i, it) in self.iters.iter_mut().enumerate() {
-            let sample = it.next();
-            if sample.is_none() {
-                return None;
-            }
-            result[i] = sample;
-        }
-        Some(result.map(|x| x.unwrap()))
-    }
-}
