4b13e06de319fa67f3171a23127ff446b1f3da3f
Daniel Friedman <dfriedman58@gmail.com>
2026-02-03 10:34:11 -0500

    Make `SharedArrayBuffer` available
    Not actually using right now, but may need for
    efficient main <-> audio thread comms that don't
    fit neatly into audio params or `postMessage`.

--- granular-app/vite.config.ts
+++ granular-app/vite.config.ts
@@ -5,4 +5,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   build: { target: "esnext" },
+  // Makes `SharedArrayBuffer` available. See https://github.com/chaosprint/vite-plugin-cross-origin-isolation/issues/3#issuecomment-1126879870
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
