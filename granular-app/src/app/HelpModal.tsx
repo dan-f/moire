cd2c709e1ce84ce666744218ab94f8e06d1db8d8
Daniel Friedman <dfriedman58@gmail.com>
2026-03-03 11:24:07 -0500

    Name project

--- granular-app/src/app/HelpModal.tsx
+++ granular-app/src/app/HelpModal.tsx
@@ -21,23 +21,28 @@ export const HelpModal = forwardRef<HelpModalRef, object>((_, ref) => {
       <TextButton className={style["close-btn"]} onClick={close}>
         {i18n("Close")}
       </TextButton>
-      <p>{import.meta.env.VITE_PROJECT_NAME} is a granular synthesizer.</p>
       <p>
-        Start by <span className={style.em}>choosing an audio sample</span>.
-        Play the synthesizer with the computer keyboard: white keys beginning
-        with C4 are on the home row, with black keys on the row above.
+        <span style={{ fontStyle: "italic" }}>
+          {import.meta.env.VITE_PROJECT_NAME}
+        </span>{" "}
+        is a polyrhythmic granular synthesizer.
       </p>
       <p>
-        Enable additional playback streams by toggling them on. Each stream
-        contributes grains at an adjustable rhythmic{" "}
+        Start by <span className={style.em}>choosing an audio sample</span>.
+        Play notes with the computer keyboard: white keys are on the home row,
+        with black keys on the row above.
+      </p>
+      <p>
+        Enable playback streams by toggling them on. Each stream contributes
+        grains at an adjustable rhythmic{" "}
         <span className={style.em}>division</span> of the main clock, which
         itself can be adjusted via <span className={style.em}>tempo</span>.
       </p>
       <p>
-        Stream parameters can be manually adjusted and automated with per-stream
-        modulation sources. Set up automation by selecting{" "}
-        <span className={style.em}>assign</span> next to a modulation source,
-        and then select a compatible parameter&apos;s target.
+        Stream parameters can be automated with per-stream modulation sources.
+        Set up automation by selecting <span className={style.em}>assign</span>{" "}
+        next to a modulation source, and then select a compatible
+        parameter&apos;s target.
       </p>
     </dialog>
   );
